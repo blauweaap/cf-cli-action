6cf07817a87bdd9adc5368726a6c42543dd6a066
marchoek <65291206+marchoek@users.noreply.github.com>
2020-06-09 16:43:46 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org y
+echo y | cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org y
 
 cf install-plugin multiapps
 
